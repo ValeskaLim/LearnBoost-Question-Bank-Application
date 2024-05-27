cc40f73cd948ed6036f2f38c47ea365fc5b5d672
ValeskaLim <valeskavalentinekklesia@gmail.com>
2024-05-27 18:38:33 +0700

    Adding modified file

---
+++ src/selection.js
@@ -0,0 +1,19 @@
+function roleSelect(event){
+    event.preventDefault();
+    const role = document.getElementById('roles').value;
+    const errorMsg = document.getElementById('error');
+
+    if(!role){
+        errorMsg.textContent = "Please select the role";
+        return false;
+    }
+
+    console.log('Role selected:', role);
+    
+    if(role == 'student')
+        window.location.href = 'loginstudent.php';
+    else if(role == 'teacher')
+        window.location.href = 'loginteacher.php';
+
+    return false;
+}
